b27faa3c3daa28dfc3b8a0b68405c730c45c6c54
Ugochukwu Onah <Fabulousonah@gmail.com>
2023-11-17 16:12:10 +0100

    Latest

--- layouts/Faq.js
+++ layouts/Faq.js
@@ -6,15 +6,15 @@ function Faq({ data }) {
   return (
     <section className="section">
       <div className="container">
-        {markdownify(title, "h1", "text-center font-normal ")}
+        {markdownify(title, "h1", "text-center font-normal")}
         <div className="section row  -mt-6">
           {faqs.map((faq, index) => (
             <div key={index} className="col-12 mt-6 md:col-6">
               <div className="p-12  shadow">
                 <div className="faq-head relative">
-                  {markdownify(faq.title, "h4")}
+                  {markdownify(faq.title, "h4", "text-primary")}
                 </div>
-                {markdownify(faq.answer, "p", "faq-body mt-4")}
+                {markdownify(faq.answer, "p", "faq-body mt-4 text-gray-300")}
               </div>
             </div>
           ))}
